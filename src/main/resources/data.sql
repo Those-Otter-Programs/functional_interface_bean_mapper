dae8b31096055a68acb097112d0fc5a5070bd3f9
James Mallon <jamesmallondev@gmail.com>
2025-03-02 21:43:50 -0300

    adding database schema.sql and data.sql for database structure and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `members` VALUES 
+    (1, 'Rocco', 'rocco@test.com'),
+    (2, 'James', 'james@test.com'),
+    (3, 'David', 'david@test.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL, 
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB; 
